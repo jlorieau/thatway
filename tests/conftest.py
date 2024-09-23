26c30e3fc7eb09f5119fc1b862785ac944c54673
J Lorieau <justin@lorieau.com>
2024-09-23 11:50:44 -0500

    Remove unused fixture, add type annotations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,13 @@
+from typing import Iterator
+
 import pytest
 
-from thatway import Config
+from thatway.manager import SettingsNamespace
 from thatway.manager import settings as s
 
 
-@pytest.fixture(autouse=True)
-def config():
-    """Retrieve and reset the config object"""
-    Config._instance = None
-    config = Config()
-    return config
-
-
 @pytest.fixture
-def settings():
+def settings() -> Iterator[SettingsNamespace]:
     """Retrieve and reset the settings object"""
     s.clear()
     yield s
