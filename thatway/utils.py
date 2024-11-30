7d7014260878bd26bcba7122b45b00556c7d83b5
J Lorieau <justin@lorieau.com>
2024-11-30 08:45:02 -0600

    Add docstring descriptions

--- thatway/utils.py
+++ thatway/utils.py
@@ -8,7 +8,13 @@ __all__ = ("clear", "locate")
 
 
 def clear(settings: SettingsManager | None = None) -> None:
-    """Clear the settings and managers for the given SettingsManager"""
+    """Clear the settings and managers for the given SettingsManager.
+
+    Parameters
+    ----------
+    settings
+        The settings manager to clear (reset) the settings
+    """
     settings = settings if settings is not None else SettingsManager()
 
     deletable_items = list(settings)
@@ -18,10 +24,20 @@ def clear(settings: SettingsManager | None = None) -> None:
         delattr(settings, attr)
 
 
-def locate(
-    settings: SettingsManager | None = None, level: int = 0, pprint: bool = True
-) -> dict[str, dict[int, Setting]]:
-    """Reveal the module locations for different settings"""
+def locate(settings: SettingsManager | None = None) -> dict[str, dict[int, Setting]]:
+    """Locate the instantiation location of settings in source files.
+
+    Parameters
+    ----------
+    settings
+        The settings manager object to analyze for locations
+
+    Returns
+    -------
+    locations
+        A dict with source code filepathss as keys with dicts as values of
+        linenumbers/settings.
+    """
     settings = settings if settings is not None else SettingsManager()
 
     # Organize the settings
@@ -31,17 +47,8 @@ def locate(
             filename, lineno = getattr(item, "__location__", ("UNKNOWN", -1))
             locations.setdefault(filename, dict())[lineno] = item
         elif isinstance(item, SettingsManager):
-            locations.update(locate(settings=item, level=level + 1))
+            locations.update(locate(item))
         else:
             continue
-
-    # Pretty-print, if desired
-    if pprint and level == 0:
-        for filename in sorted(locations.keys()):
-            print(filename)
-
-            for lineno in sorted(locations[filename].keys()):
-                item = locations[filename][lineno]
-                print(f"  {lineno}: {item}")
 
     return locations
